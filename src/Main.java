ad9ea3baacdd8ec5c9209fa20cb3ba308f1baac3
IVAN-MIXEEV <mikheeff.iw@mail.ru>
2025-03-02 13:12:00 +0300

    DZ-9 исправленная

--- src/Main.java
+++ src/Main.java
@@ -7,26 +7,19 @@ public class Main {
         task3();
         task4();
     }
-    public static int[] generateRandomArray() {
-        java.util.Random random = new java.util.Random();
-        int[] arr = new int[30];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = random.nextInt(100_000) + 100_000;
-        }
-        return arr;
-    }
     public static void task1() {
         System.out.println("Задача 1.");
-        int[] arr = generateRandomArray();
+        int[] arr = {66666,66666,66666,66666,66666};
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             sum = sum + arr[i];
         }
-        System.out.println("Сумма трат за месяц составила " +sum+" рублей");
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
     }
+
     public static void task2() {
         System.out.println("Задача 2.");
-        int[] arr = generateRandomArray();
+        int[] arr = {13332,13333,13334,13333,13332};
         int min = arr[0];
         int max = 0;
         for (int i : arr) {
@@ -40,17 +33,18 @@ public class Main {
         System.out.println("Максимальная сумма трат за день составила " + max + " рублей");
         System.out.println("Минимальная сумма трат за день составила " + min + " рублей");
     }
+
     public static void task3() {
         System.out.println("Задача 3.");
-        int[] arr = generateRandomArray();
+        int[] mounth = {13332,13333,13334,13333,13332};
         int sum = 0;
-        int month = 30;
-        for (int j :arr){
-            sum +=j;
+        for (int i = 0; i < mounth.length; i++) {
+            sum += mounth[i];
         }
-        float middle = (float) sum / month;
-        System.out.println("Средняя сумма трат за месяц составила " + middle + " рублей ");
+            sum = sum / 5;
+        System.out.println("Средняя сумма трат за месяц составила " + sum + " рублей ");
     }
+
     public static void task4() {
         System.out.println("Задача 4.");
         char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
